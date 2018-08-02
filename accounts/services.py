97e6c8003e8500c6108c91b1927387d7b7244236
artem <bulean@mail.ru>
2018-08-02 19:08:28 +0300

    add test for move_money

--- accounts/services.py
+++ accounts/services.py
@@ -23,45 +23,51 @@ class AccountService():
     # перевод средств со счета на счета - одна транзакция
     @transaction.atomic
     def move_money(self, idFrom, listIdsTo, amount):
-
-        logger.debug("call move_money:" + "idFrom = " + str(idFrom) + ";" + "listIdsTo = " + str(
-            listIdsTo) + ";" + "amount =" + str(amount))
-
-        # проверим что есть список аккаунтов куда переводить не пуст
-        lengthList = len(listIdsTo)
-        if lengthList == 0:
-            return -1
-
-        # счет с которого списываем не должен быть среди тех на кого переводим
-        if idFrom in listIdsTo:
-            return -1
-
-        print("cnt = " + str(Account.objects.filter(id__in=listIdsTo).count()))
-
-        # проверим что сумма положительная
-        if amount <= 0:
-            return -1
-
-        # достанем аккаунт с которого нужно списать
         try:
-            accountFrom = Account.objects.get(pk=idFrom)
-        except Account.DoesNotExist:
-            return -1
+            logger.debug("call move_money:" + "idFrom = " + str(idFrom) + ";" + "listIdsTo = " + str(
+                listIdsTo) + ";" + "amount =" + str(amount))
 
-        if self.check_balance(accountFrom.balance, amount):
-            sum_to_move = self.get_sum_to_each_account(amount, lengthList)
-            self.decrease_balance_on_account(accountFrom, sum_to_move * lengthList)
-        else:
-            return -1
+            # проверим что есть список аккаунтов куда переводить не пуст
+            lengthList = len(listIdsTo)
+            if lengthList == 0:
+                return -1
 
-        try:
-            with transaction.atomic():
-                accountFrom.save()
-                Account.objects.filter(id__in=listIdsTo).update(balance=F('balance') + sum_to_move)
-                return 1
+            # счет с которого списываем не должен быть среди тех на кого переводим
+            if idFrom in listIdsTo:
+                return -2
+
+            # проверим что сумма положительная
+            if amount <= 0:
+                return -3
+
+            # достанем аккаунт с которого нужно списать, блокируем запись
+            try:
+                accountFrom = Account.objects.select_for_update().get(pk=idFrom)
+            except Account.DoesNotExist:
+                return -4
+
+            # проверим что на счете достаточно средств
+            if self.check_balance(accountFrom.balance, amount):
+                sum_to_move = self.get_sum_to_each_account(amount, lengthList)
+                self.decrease_balance_on_account(accountFrom, sum_to_move * lengthList)
+            else:
+                return -5
+
+            # проверим что все переданные id для перевода существуют и заблокируем
+            if Account.objects.select_for_update().filter(id__in=listIdsTo).count() != lengthList:
+                return -6
+
+            try:
+                with transaction.atomic():
+                    accountFrom.save()
+                    Account.objects.filter(id__in=listIdsTo).update(balance=F('balance') + sum_to_move)
+                    return 1
+            except DatabaseError:
+                logger.error("error on move money on update data")
+                return -7
         except DatabaseError:
-            logger.error("error on move money")
-            return -1
+            logger.error("error on move money before update data")
+            return -8
 
     # проверка баланса на счету
     def check_balance(self, accountBalance, checkSum):
@@ -78,3 +84,9 @@ class AccountService():
     # уеньшаем баланс на аккаунте на указаную сумму
     def decrease_balance_on_account(self, account, addSum):
         account.balance = Decimal(account.balance - Decimal(addSum)).quantize(Decimal("1.00"))
+
+    def listIdsUnique(self, listIds):
+        checkSet = set()
+        for x in listIds:
+            checkSet.add(x)
+
--- accounts/tests.py
+++ accounts/tests.py
@@ -12,7 +12,10 @@ from accounts.services import AccountService
 class AccountServiceTests(TestCase):
 
     def setUp(self):
-      Account.objects.create(first_name="lion", last_name="roar", inn=1234567, balance=100)
+        Account.objects.create(pk=1, first_name="lion1", last_name="roar1", inn=1231, balance=100)
+        Account.objects.create(pk=2, first_name="lion2", last_name="roar2", inn=1232, balance=50)
+        Account.objects.create(pk=3, first_name="lion3", last_name="roar3", inn=1233, balance=0)
+        Account.objects.create(pk=4, first_name="lion4", last_name="roar4", inn=1234, balance=10)
 
     def test_check_balance_on_account(self):
         # test_account = Account.objects.get(inn=1234567)
@@ -28,8 +31,7 @@ class AccountServiceTests(TestCase):
         self.assertEquals(AccountService().get_sum_to_each_account(1, 3), 0.33)
 
     def test_decrease_balance_on_account(self):
-        account = Account.objects.get(inn__exact=1234567)
-        account.balance = 100
+        account = Account.objects.get(pk=1)
         AccountService().decrease_balance_on_account(account, 10)
         self.assertEquals(account.balance, 90)
         AccountService().decrease_balance_on_account(account, 0)
@@ -37,9 +39,52 @@ class AccountServiceTests(TestCase):
         AccountService().decrease_balance_on_account(account, 0.01)
         self.assertEquals(account.balance, Decimal(89.99).quantize(Decimal("1.00")))
 
-    #todo
     def test_move_money(self):
-        self.assertEquals(True,False)
 
+        account1 = Account.objects.get(pk=1)
+        account2 = Account.objects.get(pk=2)
+        account3 = Account.objects.get(pk=3)
+        account4 = Account.objects.get(pk=4)
+
+        result = AccountService().move_money(account1.id, [account2.id, account3.id, account4.id], 1)
+
+        # перечитаем объекты
+        account1 = Account.objects.get(pk=1)
+        account2 = Account.objects.get(pk=2)
+        account3 = Account.objects.get(pk=3)
+        account4 = Account.objects.get(pk=4)
+
+        self.assertEquals(result > 0, True)
+
+        print("account1.balance =" + str(account1.balance))
+
+        self.assertEquals(account1.balance, Decimal(99.01).quantize(Decimal("1.00")))
+        self.assertEquals(account2.balance, Decimal(50.33).quantize(Decimal("1.00")))
+        self.assertEquals(account3.balance, Decimal(0.33).quantize(Decimal("1.00")))
+        self.assertEquals(account4.balance, Decimal(10.33).quantize(Decimal("1.00")))
+
+        # проверим на пустой список
+        result = AccountService().move_money(account1.id, [], 1)
+        self.assertEquals(result, -1)
+
+        # счет с которого переводим не может быть среди тех на который переводим
+        result = AccountService().move_money(account1.id, [account1.id], 1)
+        self.assertEquals(result, -2)
+
+        # положительная сумма перевода
+        result = AccountService().move_money(account1.id, [account2.balance], 0)
+        self.assertEquals(result, -3)
+
+        # счет с которого списываем не существует
+        result = AccountService().move_money(0, [account2.balance], 1)
+        self.assertEquals(result, -4)
+
+        # на счете достаточно средств
+        result = AccountService().move_money(account1.id, [account2.id], 999)
+        self.assertEquals(result, -5)
+
+        # все получатели существуют
+        result = AccountService().move_money(account1.id, [0], 1)
+        self.assertEquals(result, -6)
 
 
